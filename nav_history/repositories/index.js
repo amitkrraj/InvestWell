bd7f3a93991ca595489501d5d7cf1af14c7d2c1f
amitkrraj <amitkrraj.cse@gmail.com>
2022-02-28 13:36:33 +0530

    update

--- nav_history/repositories/index.js
+++ nav_history/repositories/index.js
@@ -15,11 +15,10 @@ const getCategory = async attributes => {
     return result
 }
 
-const getschemesDetails = async attributes => {
+const getschemesDetails = async (attributes) => {
     const result = await db.sequelize.query(
-        `select name, corpus, 6Month, 1Year, 2Year, 3Year, 5Year, 10Year from schemes INNER JOIN factsheets ON schemes.fsid = factsheets.fsid INNER JOIN schemereturns ON schemes.schid = schemereturns.schid where schemes.schid = ? and schemes.fsid = ?`,
+        `select name, corpus, 6Month, 1Year, 2Year, 3Year, 5Year, 10Year from schemes INNER JOIN factsheets ON schemes.fsid = factsheets.fsid INNER JOIN schemereturns ON schemes.schid = schemereturns.schid where schemes.schid = ${attributes.schid} and schemes.fsid = ${attributes.fsid}`,
         {
-            replacements: [attributes.schid, attributes.fsid],
             type: db.Sequelize.QueryTypes.SELECT
         }
     )
